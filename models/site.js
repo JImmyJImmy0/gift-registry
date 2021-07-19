04a7f4583e7511f2ebfd62648b8189f0d2710a85
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 10:01:12 -0600

    create site schema

---
+++ models/site.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export {
+    Site,
+}
+
+const siteSchema = new Schema ({
+    name: String,
+    info: [infoSchema],
+    owner: {type: Schema.Types.ObjectId, 'ref': 'Profile'}
+})
+
+const Site = mongoose.model('Site', siteSchema)
