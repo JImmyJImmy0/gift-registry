3044b40df6786de3d5808f833cf524f38da5ddcb
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 12:57:33 -0600

    make info schema

--- models/site.js
+++ models/site.js
@@ -5,9 +5,21 @@ export {
     Site,
 }
 
+const infoSchema = new Schema ({
+    name: String,
+    description: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    link: String,
+    site: {type: mongoose.Schema.Types.ObjectId, ref: 'Site'}
+})
+
 const siteSchema = new Schema ({
     name: String,
-    // info: [infoSchema],
+    info: [infoSchema],
     owner: {type: Schema.Types.ObjectId, 'ref': 'Profile'}
 })
 
