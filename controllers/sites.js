932c8b4ee40e5ca760096b659813b12314603938
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 10:59:20 -0600

    write create controller function

--- controllers/sites.js
+++ controllers/sites.js
@@ -2,6 +2,19 @@ import { Site } from "../models/site.js";
 
 export {
     index,
+    create,
+}
+
+function create(req, res) {
+    req.body.owner = req.user.profile
+    Site.create(req.body)
+    .then(site => {
+        res.redirect('/sites')
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/sites')
+    })
 }
 
 function index(req, res) {
