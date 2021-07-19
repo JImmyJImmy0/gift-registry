a71108e436add6764407033cb95a245161330292
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 17:30:54 -0600

    define route to edit ctrl func

--- routes/sites.js
+++ routes/sites.js
@@ -11,5 +11,6 @@ const router = Router()
 
 router.get('/', sitesCtrl.index)
 router.get('/:id', sitesCtrl.show)
+router.get('/:id/edit', isLoggedIn, sitesCtrl.edit)
 router.post('/', isLoggedIn, sitesCtrl.create)
 router.post('/:id/description', isLoggedIn, sitesCtrl.addDescription)
