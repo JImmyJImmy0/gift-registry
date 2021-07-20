d1cec79dfe502ac1f00e306dfff5b33c322df8aa
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 18:53:00 -0600

    write list schema and emmbed in profile

--- models/profile.js
+++ models/profile.js
@@ -4,9 +4,19 @@ export {
   Profile
 }
 
+const listSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  // image: Image,
+  note: String,
+  priority: Number,
+  url: String, 
+})
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  list: [listSchema]
 }, {
   timestamps: true
 })
