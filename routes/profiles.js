af8a9584c834cfe4c8ae724af81b07e2d7904d47
JImmyJImmy0 <jerickson317@gmail.com>
2021-07-19 18:19:31 -0600

    stub profile index router

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 export {
     router
@@ -6,3 +7,4 @@ export {
 
 const router = Router()
 
+router.get('/', isLoggedIn, )
